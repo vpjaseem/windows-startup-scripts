334ea8d4a1c110fa8f9dae5e147dc859134751af
vpjaseem <67306692+vpjaseem@users.noreply.github.com>
2020-07-08 03:51:53 +0530

    Add files via upload

---
+++ 2-download-web-app-extract.ps1
@@ -0,0 +1,28 @@
+﻿#Download the Web App file
+cd C:\inetpub\wwwroot
+$url = "https://github.com/vpjaseem/sample-static-web-app/archive/master.zip"
+$output = "C:\inetpub\wwwroot\master.zip"
+$start_time = Get-Date
+
+Invoke-WebRequest -Uri $url -OutFile $output
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+#Uzip the Web App master zip file
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+Unzip "C:\inetpub\wwwroot\master.zip" "C:\inetpub\wwwroot\master"
+
+#Moving Web App contents to wwwroot of IIS
+cd C:\inetpub\wwwroot\master\sample-static-web-app-master
+Move-Item -Path .\* -Destination C:\inetpub\wwwroot\
+cd C:\inetpub\wwwroot\
+
+#Removing unwanted folders
+Remove-Item -LiteralPath "C:\inetpub\wwwroot\master\" -Force -Recurse
+Remove-Item "C:\inetpub\wwwroot\master.zip"
