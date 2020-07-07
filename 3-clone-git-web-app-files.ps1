5ca57e4d41e9af8834ffe0aedf68100a422c5fca
vpjaseem <67306692+vpjaseem@users.noreply.github.com>
2020-07-08 03:51:15 +0530

    Delete 3-clone-git-web-app-files.ps1

--- 3-clone-git-web-app-files.ps1
+++
@@ -1,6 +0,0 @@
-﻿cd C:\inetpub\wwwroot\
-git clone https://github.com/vpjaseem/sample-static-web-app.git
-cd C:\inetpub\wwwroot\sample-static-web-app\
-Move-Item -Path .\* -Destination C:\inetpub\wwwroot\
-cd C:\inetpub\wwwroot\
-Remove-Item -LiteralPath "C:\inetpub\wwwroot\sample-static-web-app\" -Force -Recurse
